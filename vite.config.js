4bb937194b5ad45a89228377ca974c7c9a084ee7
Office50505 <officeforoffice9999@gmail.com>
2026-08-02 17:54:17 +0530

    Bust mobile stylesheet cache

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash]-m[extname]'
+      }
+    }
+  },
   server: {
     port: 5173,
     watch: {
